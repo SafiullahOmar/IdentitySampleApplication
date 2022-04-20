3c6f91d0bb23d71b1f603abf06eed39540bdfca1
Safiullah Omar <omar.safiullah@gmail.com>
2022-04-20 17:41:31 +0430

    User creation on Demand

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Identity.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -14,9 +15,51 @@ namespace Identity.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+
+
         }
 
-         
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            this.SeedUsers(builder);
+            this.SeedUserRoles(builder);
+        }
+
+        //private void SeedRoles(ModelBuilder builder)
+        //{
+        //    builder.Entity<IdentityRole>().HasData(
+        //        new IdentityRole() { Id = "fab4fac1-c546-41de-aebc-a14da6895711", Name = "Admin", ConcurrencyStamp = "1", NormalizedName = "Admin" },
+        //        new IdentityRole() { Id = "c7b013f0-5201-4317-abd8-c211f91b7330", Name = "HR", ConcurrencyStamp = "2", NormalizedName = "Human Resource" }
+        //        );
+        //}
+        private void SeedUsers(ModelBuilder builder)
+        {
+            ApplicationUser user = new ApplicationUser()
+            {
+                Id = "206a0346484a4fe8b5d659ccba72ef71",
+                UserName = "Admin",
+                Email = "admin@gmail.com",
+                LockoutEnabled = false,
+                PhoneNumber = "1234567890"
+            };
+
+            PasswordHasher<ApplicationUser> passwordHasher = new PasswordHasher<ApplicationUser>();
+            passwordHasher.HashPassword(user, "Admin*123");
+
+            builder.Entity<ApplicationUser>().HasData(user);
+        }
+
+        private void SeedUserRoles(ModelBuilder builder)
+        {
+            builder.Entity<IdentityUserRole<string>>().HasData(
+                new IdentityUserRole<string>() { RoleId = "3fbb699e-007f-4c01-890c-87a2edc5d7e5", UserId = "206a0346484a4fe8b5d659ccba72ef71" }
+                );
+        }
+
+
+
         public DbSet<Identity.Models.RoleStore> RoleStore { get; set; }
 
          
---
+++ Data/Migrations/20220420084603_addUser.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Identity.Data.Migrations
+{
+    public partial class addUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AppUserVM",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppUserVM", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RoleStore",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoleStore", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "FirstName", "LastName", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "206a0346484a4fe8b5d659ccba72ef71", 0, "901873e9-cb3d-44cd-9b72-c5806362f406", "admin@gmail.com", false, null, null, false, null, null, null, null, "1234567890", false, "34299476-d989-4d7c-8ead-660561725860", false, "Admin" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "3fbb699e-007f-4c01-890c-87a2edc5d7e5", "206a0346484a4fe8b5d659ccba72ef71" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppUserVM");
+
+            migrationBuilder.DropTable(
+                name: "RoleStore");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "3fbb699e-007f-4c01-890c-87a2edc5d7e5", "206a0346484a4fe8b5d659ccba72ef71" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "206a0346484a4fe8b5d659ccba72ef71");
+        }
+    }
+}
